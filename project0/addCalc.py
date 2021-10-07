e4ff15145ea13ea43cedc99b8aae09e996948bac
Charlotte Yao <charlotteyao27@gmail.com>
2021-10-07 13:04:03 -0700

    Functions Exercises

---
+++ project0/addCalc.py
@@ -0,0 +1,11 @@
+#Write a function that takes in two numbers, and adds them together. Make your function print out a sentence
+#showing the two numbers, and the result.
+#Call your function with three different sets of numbers.\
+
+def add(x, y):
+    sum = x + y
+    print('sum of ' + str(x) + ' and ' + str(y) + ' is ' + str(sum))
+
+add(1, 2)
+add(2, 3)
+add(3, 4)
